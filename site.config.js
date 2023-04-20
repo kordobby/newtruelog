266fa583f3e3787ca2ad0672ac4872de29b0c3eb
Yoon Lee <poordobby@naver.com>
2023-04-21 01:03:12 +0900

    add. static meta tags

--- site.config.js
+++ site.config.js
@@ -1,7 +1,51 @@
 export const SITE_CONFIG = {
   /* meta */
   meta: {
-    lang: 'ko-KR',
+    title: 'Next.js v13 Guide',
+    description: 'Next.js v13 Template made by LEETRUE',
+    /* Basic Fields */
+    generator: 'Next.js',
+    applicationName: 'Next.js v13',
+    referrer: 'origin-when-cross-origin', //
+    keywords: ['Next.js', 'React', 'JavaScript'],
+    creator: 'LEETRUE',
+    publisher: 'LEETRUE',
+    medtadataBase: new URL('https://yoon.com'),
+    alternates: {
+      canonical: '/',
+      languages: {
+        'en-US': '/en-US',
+        'ko-KR': '/ko-KR',
+      },
+    },
+    authors: [{ name: 'LEETRUE' }],
+    robots: {
+      index: true,
+      follow: true,
+      googleBot: {
+        index: true,
+        follow: true,
+      },
+    },
+    openGraph: {
+      type: 'website',
+      title: 'dynamic title',
+      description: 'dynamic description',
+      url: 'dynamic url',
+      siteName: 'LEETRUE BLOG',
+      locale: 'ko_KR',
+      images: [
+        {
+          url: 'imageURL',
+          width: 400,
+          height: 250,
+          alt: 'images for OG',
+          type: 'image/jpeg',
+          secureUrl: 'secureUrl',
+        }, // DYNAMIC
+      ],
+      authors: ['LEETRUE'],
+    },
   },
   /* blog */
   blog: {
