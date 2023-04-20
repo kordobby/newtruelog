9c22d071e1dedb3e02f910f67c4566f1b2db748c
Yoon Lee <poordobby@naver.com>
2023-04-20 22:10:43 +0900

    add. site config init

---
+++ site.config.js
@@ -0,0 +1,38 @@
+const SITE_CONFIG = {
+  /* meta */
+  meta: {
+    lang: 'ko-KR',
+  },
+  /* blog */
+  blog: {
+    title: 'truelog',
+    desc: 'dobby is free',
+    since: '2022',
+  },
+  /* profile */
+  profile: {
+    name: 'leetrue',
+    image: '',
+    role: 'Frontend Developer',
+    github: 'kordobby',
+    instagram: 'yuniscells',
+    careerly: 'yoon',
+  },
+  projects: [
+    {
+      name: 'CHIC',
+      href: '',
+    },
+  ],
+  seo: {
+    keywords: ['Blog', 'Website', 'Notion'],
+  },
+  profile: {},
+  notionConfig: '',
+  gaConfig: '',
+  utterances: {
+    config: {
+      repository: 'kordobby/truelog',
+    },
+  },
+};
