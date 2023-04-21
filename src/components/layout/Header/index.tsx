c0dc41036d711c9996a0fb01fa3bebaf2a0ca4d3
Yoon Lee <poordobby@naver.com>
2023-04-21 19:19:00 +0900

    add. header

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -1,5 +1,16 @@
+'use client';
+
+import { colors } from '@/libs/global/palette';
+import styled from 'styled-components';
+
 const Header = () => {
-  return <></>;
+  return <HeaderWrapper></HeaderWrapper>;
 };
 
 export default Header;
+
+const HeaderWrapper = styled.div`
+  width: 100%;
+  height: 80px;
+  background-color: ${colors.brand.blue};
+`;
