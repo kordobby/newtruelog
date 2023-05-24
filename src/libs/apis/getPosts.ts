f8d1aee844afc58a77591f1da9fc4468ee423593
Yoon Lee <poordobby@naver.com>
2023-05-24 19:37:28 +0900

    add. apis

--- src/libs/apis/getPosts.ts
+++ src/libs/apis/getPosts.ts
@@ -5,11 +5,12 @@ export const getPosts = async () => {
   const id = 'c82b4d55012b4601b7a1021e75ed7594' as string;
   const api = new NotionAPI();
   const response = await api.getPage(id);
+
+  const collection = Object.values(response.collection)[0]?.value;
+  const block = response.block;
+  const schema = collection?.schema;
   const uuid = idToUuid(id);
+  const rawMetadata = block[uuid].value;
 
-  // const collection = Object.values(response.collection)[0]?.value;
-  // const block = response.block;
-  // const schema = collection?.schema;
-
-  // const rawMetadata = block[uuid].value;
+  console.log('roaw', rawMetadata);
 };
