f9f3aee818e6ec9e3936cdeaffb6bc2211b1ff6f
Yoon Lee <poordobby@naver.com>
2023-06-01 22:14:34 +0900

    Refactor. filtering posts by tag logic refactor

--- src/components/Feed/PostCardList/index.tsx
+++ src/components/Feed/PostCardList/index.tsx
@@ -3,17 +3,25 @@
 import { TPosts } from '@/libs/types';
 import { FC, useEffect, useState } from 'react';
 import PostCard from '../PostCard';
+import { useSearchParams } from 'next/navigation';
 
 interface PostCardListInterface {
   posts?: TPosts;
 }
 const PostCardList: FC<PostCardListInterface> = ({ posts }) => {
   const [filterdPosts, setFilteredPosts] = useState<TPosts>([]);
+  const searchParams = useSearchParams();
+  const tag = searchParams.get('t');
 
   useEffect(() => {
     if (!posts) return;
+    if (tag !== null) {
+      const newArray = posts.filter((value) => value?.tags?.includes(tag));
+      setFilteredPosts(newArray);
+      return;
+    }
     setFilteredPosts(posts);
-  }, [posts]);
+  }, [posts, tag]);
 
   if (!posts) return <></>;
   return (
